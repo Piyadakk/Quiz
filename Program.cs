3896df59bdefd1b084f6d0c44cc40e14e8093b7d
ttmicku <wun002002@gmail.com>
2023-04-24 10:52:25 +0700

    help

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿class Program {
+    static void Main(string[] args) {
+        Console.Write("input number: ");
+        float number = float.Parse(Console.ReadLine());
+
+        Console.Write("input item: ");
+        string inputItem = Console.ReadLine();
+        
+        Console.Write("input itemtype (except 'ShowAll'): ");
+        string inputItemtype = Console.ReadLine();
+
+        if (inputItemtype  == "ShowAll") {
+            Console.WriteLine("All Items:");
+        }
+        else {
+
+            Console.Write("input item: ");
+            string inputItems = Console.ReadLine();
+
+            Console.Write("input itemtype: ");
+            string inputItemtypes = Console.ReadLine();{ 
+        }
+        }
+    }
+}
+    
+
